dfb83773528fc14ed58f9d29b52c98070bf19803
Arthur Gunn <arthur@gunn.co.nz>
2009-11-28 21:31:01 +1300

    Initial commit. Just a blank plugin. Soon to have table generating fun.

---
+++ tasks/details_table_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :details_table do
+#   # Task goes here
+# end
---
+++ test/details_table_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class DetailsTableTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
